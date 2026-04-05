cdd28b93be2a69d36126fed3ebafbfef1ea89724
Asali Khokhar <Furqan.Khokhar@city.ac.uk>
2026-04-05 21:40:22 +0100

    feat: organise promotion database class connection
    This commit comments out database connection code and places promotion.db database into data file to separate from other files.

--- IPOS-PU/app/prm/src/main/java/ac/csg/in2033/ipos/pu/prm/Database.java
+++ IPOS-PU/app/prm/src/main/java/ac/csg/in2033/ipos/pu/prm/Database.java
@@ -3,16 +3,35 @@ package ac.csg.in2033.ipos.pu.prm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Database {
     private final static Logger logger = LoggerFactory.getLogger(Database.class);
+
+    private static final String DB_DIR = System.getProperty("user.dir") + "/data";
     private static final String DB_NAME = "promotions.db"; // separate DB
 
     public static Connection connect() {
         try {
-            String url = "jdbc:sqlite:" + DB_NAME;
+            // Define data directory
+            File dir = new File(DB_DIR);
+            // If directory does not exist
+            if (!dir.exists()) {
+                // Create data directory
+                // If data directory cannot be created
+                if (!dir.mkdirs()) {
+                    // Log failure to create directory
+                    logger.error("Failed to create database directory: {}", dir.getAbsolutePath());
+                }
+            }
+
+            // Create prm database file
+            File dbFile = new File(dir, DB_NAME);
+
+            // Return connection to database
+            String url = "jdbc:sqlite:" + dbFile.getAbsolutePath();
             return DriverManager.getConnection(url);
 
         } catch (Exception e) {
